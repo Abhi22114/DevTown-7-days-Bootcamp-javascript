21bcae66198070952837bc65af761e86d804f340
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2023-01-12 23:19:39 +0530

    Update 2nd day comments.js

---
+++ 2nd day comments.js
@@ -0,0 +1,15 @@
+// single line comments 
+// comments in javascript are written with the help of (//) forward salsh
+
+// multilines comments :---->>>
+// in javascript multiline comments are written with /* and end with */.
+//-->. Any text between /* and */ will be ignored by JavaScript.
+// This example uses a comment block to prevent execution of multiple lines:
+
+// shortcut for writting comments 
+//we use [ctrl+/(forward slash)]
+
+
+
+// JavaScript comments can be used to explain JavaScript code, and to make it more readable.
+// JavaScript comments can also be used to prevent execution, when testing alternative code.
