1d8a0254eade3d72dbe541627d49bfa70f87200f
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2023-01-12 23:20:29 +0530

    Update 2nd day datatypes.js

---
+++ 2nd day datatypes.js
@@ -0,0 +1,49 @@
+
+document.write('Hello, World!');
+'abhijeet'
+// the hello world program
+document.write('Hello, World!');
+// string datatypes in javascript
+// It can contain anything ,and written under single or double quotation mark
+// String is used to store text. In JavaScript, strings are surrounded by quotes:
+
+// Single quotes: 'Hello'
+// Double quotes: "Hello"
+// Backticks: `Hello`
+
+//strings example
+const name = 'ram';
+const name1 = "hari";
+const result = `The names are ${name} and ${name1}`;
+console.log(result);
+// "1233" is a string
+// "abhij" is string
+
+// Number 
+// Number represents integer and floating numbers (decimals and exponentials). 
+const number1 = 3/0;
+console.log(number1); // Infinity
+
+const number2 = -3/0;
+console.log(number2); // -Infinity
+
+// strings can't be divided by numbers
+const number3 = "abc"/3; 
+console.log(number3);  // NaN
+
+// Boolean datatypes ,its datatypes in javascript that print the value in true or false value 
+// examples
+// This data type represents logical entities. Boolean represents one of two values: true or false. It is easier to think of it as a yes/no switch. For example,
+
+// const dataChecked = true;
+// const valueCounted = false;
+
+typeof("Raja");
+x=7
+7
+y=8
+8
+console.log(x>y);
+// boolean
+console.log(x<y);
+//rules of varriables in javascript
