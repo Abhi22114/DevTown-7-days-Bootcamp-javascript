9b728e9c8144056d543307c7efb3974f5d7e4388
Abhijeet Kumar <114794064+Abhi22114@users.noreply.github.com>
2023-01-12 23:15:37 +0530

    Update 1st day.js

--- 1st day.js
+++ 1st day.js
@@ -1,2 +1,27 @@
 console.log("Hello world");
+// JavaScript is the world's most popular programming language.
+
+// JavaScript is the language of the Web.
+
+// JavaScript is easy to learn.
+// JavaScript is a powerful and flexible programming language.
+// It can execute on a web browser that allows us to make interactive webpages 
+//such as popup menus, animations, form validation, etc.
+
+
+// How to run program in javascript
+
+// 1.using console.log
+// // the hello world program
+console.log('Hello World');
+// 2.using alert
+// the hello world program
+alert("Hello, World!");
+
+// 3.using document.write method
+// the hello world program
+document.write('Hello, World!');
+'abhijeet'
+// the hello world program
+document.write('Hello, World!');
 
